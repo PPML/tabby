e35926161314d61a5338e5fff6a3ae0275fe43cf
Christian Testa <testa101@gmail.com>
2018-01-25 20:17:59 -0500

    Catching a typo.

---
+++ utilities/R/description-panel.R
@@ -0,0 +1,31 @@
+emptyDescriptionPanel <- function() {
+  tags$div(
+    class="about-tab tab-pane"
+  )
+}
+
+emptyEstimatesPanel <- function() {
+  tags$div(
+    class="estimates-tab tab-pane"
+  )
+}
+
+emptyTrendsPanel <- function() {
+  tags$div(
+    class="trends-tab tab-pane"
+  )
+}
+
+
+emptyAgeGroupsPanel <- function() {
+  tags$div(
+    class="agegroups-tab tab-pane"
+  )
+}
+
+descriptionPanel <- function() {
+  tags$div(
+    class="about-tab tab-pane active",
+    "This is an initial description page to be filled out later."
+  )
+}
