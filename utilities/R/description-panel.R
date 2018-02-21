0d4949f0253926aa7b1e81804c94c6ba3815c97b
Christian Testa <testa101@gmail.com>
2018-02-21 17:07:39 -0500

    Italicize and properly capitalize M. tb.

--- utilities/R/description-panel.R
+++ utilities/R/description-panel.R
@@ -86,9 +86,9 @@ descriptionPanel <- function() {
     h4("Additional Scenarios"),
 
     tags$ul(
-      tags$li("'No Transmission Within the United States after 2016': From 2016 onwards, no individuals acquire M. TB infection from transmission within the United States. In this scenario the only source of new LTBI cases is from existing infection in new migrants entering the country."),
+      tags$li("'No Transmission Within the United States after 2016': From 2016 onwards, no individuals acquire ", em("M. tb"), " infection from transmission within the United States. In this scenario the only source of new LTBI cases is from existing infection in new migrants entering the country."),
 
-      tags$li("'No LTBI Among Immigrants after 2016': From 2016 onwards, all individuals immigrating to the United States are free of M. tb infection, while maintaining the same total volume of immigration.")
+      tags$li("'No LTBI Among Immigrants after 2016': From 2016 onwards, all individuals immigrating to the United States are free of ", em("M. tb"), " infection, while maintaining the same total volume of immigration.")
 
     ),
 
