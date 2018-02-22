dc12a59a1e307e21f87452103d1e0d99b8f8fa3f
Christian Testa <testa101@gmail.com>
2018-02-22 16:55:09 -0500

    Add a period.

--- utilities/R/description-panel.R
+++ utilities/R/description-panel.R
@@ -98,7 +98,7 @@ descriptionPanel <- function() {
 
     p("Section 508 requires Federal agencies and grantees receiving Federal funds to ensure that individuals with disabilities who are members of the public or Federal employees have access to and use of electronic and information technology (EIT) that is comparable to that provided to individuals without disabilities, unless an undue burden would be imposed on the agency."),
 
-    p("If you need assistance with this web application, please contact ", tags$a("ppml@hsph.harvard.edu", href='mailto:ppml@hsph.harvard.edu'))
+    p("if you need assistance with this web application, please contact ", tags$a("ppml@hsph.harvard.edu", href='mailto:ppml@hsph.harvard.edu'), ".")
 
   )
 }
